f6032a59c9d0cda6209d536c5a19831ed1ac121a
Jeremy Othieno <j.othieno@gmail.com>
2016-05-15 21:35:51 +0200

    Add stubs for the GraphicsEngine and TaskManager class

--- clockwork.pro
+++ clockwork.pro
@@ -13,30 +13,35 @@ MOC_DIR = $${DESTDIR}/moc
 RCC_DIR = $${DESTDIR}/rcc
 UI_DIR = $${DESTDIR}/ui
 
-INCLUDEPATH += src \
-               src/concurrency \
-               src/graphics \
-               src/graphics/renderer \
-               src/io \
-               src/math \
-               src/scene \
-               src/system \
-               src/ui
+INCLUDEPATH += \
+    src/graphics \
+    src/graphics/renderer \
+    src/io \
+    src/math \
+    src/scene \
+    src/system \
+    src/task \
+    src/ui \
+    src
 
 HEADERS += \
-           src/graphics/Framebuffer.hh \
-           src/ui/FramebufferProvider.hh \
-           src/ui/UserInterface.hh \
-           src/system/Application.hh \
-           src/system/Error.hh \
-           src/system/Service.hh \
-           src/clockwork.hh
+    src/graphics/Framebuffer.hh \
+    src/graphics/GraphicsEngine.hh \
+    src/system/Application.hh \
+    src/system/Error.hh \
+    src/system/Service.hh \
+    src/task/TaskManager.hh \
+    src/ui/FramebufferProvider.hh \
+    src/ui/UserInterface.hh \
+    src/clockwork.hh
 
 SOURCES += \
-           src/graphics/Framebuffer.cc \
-           src/ui/FramebufferProvider.cc \
-           src/ui/UserInterface.cc \
-           src/system/Application.cc \
-           src/system/Error.cc \
-           src/system/Service.cc \
-           src/clockwork.cc
+    src/graphics/Framebuffer.cc \
+    src/graphics/GraphicsEngine.cc \
+    src/system/Application.cc \
+    src/system/Error.cc \
+    src/system/Service.cc \
+    src/task/TaskManager.cc \
+    src/ui/FramebufferProvider.cc \
+    src/ui/UserInterface.cc \
+    src/clockwork.cc
---
+++ src/graphics/GraphicsEngine.hh
@@ -0,0 +1,84 @@
+/*
+ * This file is part of Clockwork.
+ *
+ * Copyright (c) 2014-2016 Jeremy Othieno.
+ *
+ * The MIT License (MIT)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef CLOCKWORK_GRAPHICS_ENGINE_HH
+#define CLOCKWORK_GRAPHICS_ENGINE_HH
+
+#include "Framebuffer.hh"
+
+
+namespace clockwork {
+
+class TaskManager;
+class Scene;
+
+class GraphicsEngine final
+{
+public:
+    /**
+     *
+     */
+    explicit GraphicsEngine(TaskManager&);
+    /**
+     *
+     */
+    GraphicsEngine(const GraphicsEngine&) = delete;
+    /**
+     *
+     */
+    GraphicsEngine(GraphicsEngine&&) = delete;
+    /**
+     *
+     */
+    ~GraphicsEngine();
+    /**
+     *
+     */
+    GraphicsEngine& operator=(const GraphicsEngine&) = delete;
+    /**
+     *
+     */
+    GraphicsEngine& operator=(GraphicsEngine&&) = delete;
+    /**
+     * Renders the specified scene.
+     */
+    void render(const Scene& scene);
+    /**
+     * Returns the framebuffer instance.
+     */
+    Framebuffer& getFramebuffer();
+private:
+    /**
+     * The task manager.
+     */
+    TaskManager& taskManager_;
+    /**
+     * The framebuffer.
+     */
+    Framebuffer framebuffer_;
+};
+
+} // namespace clockwork
+
+#endif // CLOCKWORK_GRAPHICS_ENGINE_HH
