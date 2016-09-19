fcb993a369eaa59758caf21f12d6fcd432f511f2
Jeremy Othieno <j.othieno@gmail.com>
2016-09-19 14:16:35 +0200

    Implement a toString function overload for Framebuffer resolutions

--- src/system/io/toString.cc
+++ src/system/io/toString.cc
@@ -27,10 +27,35 @@
 
 std::string
 clockwork::toString(const Error error) {
-    switch (error) {
-        case Error::None:
-            return "None";
-        default:
-            return "???";
-    }
+	switch (error) {
+		case Error::None:
+			return "None";
+		default:
+			return "???";
+	}
+}
+
+
+std::string
+clockwork::toString(const Framebuffer::Resolution resolution) {
+	switch (resolution) {
+		case Framebuffer::Resolution::ZERO:
+			return "ZERO (0 x 0)";
+		case Framebuffer::Resolution::VGA:
+			return "VGA (640 x 480)";
+		case Framebuffer::Resolution::SVGA:
+			return "SVGA (800 x 600)";
+		case Framebuffer::Resolution::XGA:
+			return "XGA (1024 x 768)";
+		case Framebuffer::Resolution::SXGA:
+			return "SXGA (1280 x 1024)";
+		case Framebuffer::Resolution::FHD:
+			return "FHD (1920 x 1080)";
+		case Framebuffer::Resolution::QSXGA:
+			return "QSXGA (2560 x 2048)";
+		case Framebuffer::Resolution::UHD8K:
+			return "UHD8K (7680 x 4320)";
+		default:
+			return "???";
+	}
 }
