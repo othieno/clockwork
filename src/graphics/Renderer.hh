1f89f0f59a929d808b1c2dc5cd8ece2fc5b34784
Jeremy Othieno <j.othieno@gmail.com>
2016-09-30 18:39:56 +0200

    Implement the Varying attribute validator

--- src/graphics/Renderer.hh
+++ src/graphics/Renderer.hh
@@ -26,6 +26,13 @@
 #define CLOCKWORK_RENDERER_HH
 
 #include "ReferenceVariant.hh"
+#include "Point.hh"
+#include "Point3.hh"
+#include "Point4.hh"
+#include "Vector3.hh"
+#include "Vector4.hh"
+#include "Matrix4.hh"
+#include "ColorARGB.hh"
 #include <QHash>
 
 
@@ -85,11 +92,24 @@ public:
 	 */
 	using Uniform = ReferenceVariant<UniformValidator>;
 	/**
-	 * Checks whether T is a valid Varying value type.
+	 * Checks whether T is a valid Varying value type. Varying attributes are
+	 * restricted to arithmetic types, as well as Point, Point3, Point4,
+	 * Vector3, Vector4, Matrix4 or ColorARGB.
 	 */
 	template<class T>
 	struct VaryingValidator :
-	std::integral_constant<bool, true> {};
+	std::integral_constant<bool,
+		std::is_arithmetic<T>::value ||
+		std::is_base_of<Point, T>::value ||
+		std::is_base_of<Point3, T>::value ||
+		std::is_base_of<Point4, T>::value ||
+		std::is_base_of<Vector3, T>::value ||
+		std::is_base_of<math::Vector4i, T>::value ||
+		std::is_base_of<math::Vector4u, T>::value ||
+		std::is_base_of<math::Vector4f, T>::value ||
+		std::is_base_of<math::Vector4d, T>::value ||
+		std::is_base_of<Matrix4, T>::value ||
+		std::is_base_of<ColorARGB, T>::value> {};
 	/**
 	 *
 	 */
