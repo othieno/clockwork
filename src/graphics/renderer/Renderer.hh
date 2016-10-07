1f1b10d3b4a852c73a3aff1fbc70ad383ad60f60
Jeremy Othieno <j.othieno@gmail.com>
2016-10-07 17:52:18 +0200

    Implement the Renderer::draw function

--- src/graphics/renderer/Renderer.hh
+++ src/graphics/renderer/Renderer.hh
@@ -27,15 +27,13 @@
 
 #include "RenderingContext.hh"
 #include "Fragment.hh"
+#include "Framebuffer.hh"
+#include "Mesh.hh"
 #include "VertexAttributes.hh"
 #include "VertexShaderOutput.hh"
 
 
 namespace clockwork {
-/**
- * @see graphics/Mesh.hh.
- */
-class Mesh;
 /**
  *
  */
@@ -43,22 +41,115 @@ template<RenderingAlgorithm algorithm, class Implementation>
 class Renderer {
 public:
 	using Fragment = detail::Fragment<algorithm>;
+	using Fragments = QList<Fragment>;
 	using Varying = detail::Varying<algorithm>;
 	using VertexAttributes = detail::VertexAttributes<algorithm>;
 	using VertexShaderOutput = detail::VertexShaderOutput<algorithm>;
+	using VertexShaderOutputs = QList<VertexShaderOutput>;
 	/**
 	 * Renders the specified mesh in the given context.
 	 * @param context the rendering context.
 	 * @param mesh the polygon mesh to render.
 	 */
-	static void draw(RenderingContext& context, const Mesh& mesh){}
+	static void draw(RenderingContext& context, const Mesh& mesh) {
+		auto* const framebuffer = context.framebuffer;
+		if (framebuffer == nullptr || mesh.faces.isEmpty()) {
+			return;
+		}
+		auto* const pixelBuffer = framebuffer->getPixelBuffer();
+		auto* const depthBuffer = framebuffer->getDepthBuffer();
+		auto* const stencilBuffer = framebuffer->getStencilBuffer();
+
+		for (const auto& face : mesh.faces) {
+			VertexShaderOutputs vertexShaderOutputs;
+			for (std::size_t i = 0; i < face.length; ++i) {
+				// Per-vertex operation.
+				auto varying = Implementation::createVarying(face, i);
+				const auto& attributes = Implementation::createVertexAttributes(face, i);
+				auto output = Implementation::vertexShader(context.uniforms, varying, attributes);
+
+				// Attach the varying variables to the vertex shader's output for use by the
+				// fragment shader later on.
+				output.varying = std::move(varying);
+
+				vertexShaderOutputs.append(output);
+			}
+			// Primitive assembly.
+			auto& primitives = Implementation::assemble(context.primitiveMode, vertexShaderOutputs);
+
+			for (auto& primitive : primitives) {
+				// Perspective divide: Convert primitive positions from clipping
+				// coordinate space to normalized device coordinate (NDC) space.
+				auto& position = primitive.position;
+				position.x /= position.w;
+				position.y /= position.w;
+				position.z /= position.w;
+				position.w /= 1.0;
+
+				// Convert the positions from NDC space to window (screen) space.
+				position = position * context.viewportTransform;
+			}
+
+			// Rasterization.
+			for (const auto& fragment : Implementation::rasterize(context, primitives)) {
+				const int offset = context.framebuffer->getOffset(fragment.x, fragment.y);
+				if (offset >= 0 && fragmentPasses(context, fragment)) {
+					pixelBuffer[offset] = Implementation::fragmentShader(context.uniforms, fragment.varying, fragment);
+					depthBuffer[offset] = fragment.depth;
+					stencilBuffer[offset] = fragment.stencil;
+				}
+			}
+		}
+	}
 	/**
 	 * Returns true if the specified fragment passes all fragment tests, false otherwise.
 	 * @param context the rendering context.
 	 * @param fragment the fragment to test.
 	 */
-	static bool fragmentPasses(const RenderingContext& context, const Fragment& fragment){
+	static bool fragmentPasses(const RenderingContext&, const Fragment&) {
 		return true;
+	}
+	/**
+	 * Returns a dummy VertexAttributes object.
+	 * This function should be implemented in an explicit template specialization.
+	 */
+	static VertexAttributes createVertexAttributes(const Mesh::Face&, const std::size_t) {
+		return VertexAttributes();
+	}
+	/**
+	 * Returns a dummy Varying object.
+	 * This function should be implemented in an explicit template specialization.
+	 */
+	static Varying createVarying(const Mesh::Face&, const std::size_t) {
+		return Varying();
+	}
+	/**
+	 * Returns a dummy VertexShaderOutput object.
+	 * This function should be implemented in an explicit template specialization.
+	 */
+	static VertexShaderOutput vertexShader(const Uniforms&, Varying&, const VertexAttributes&) {
+		return VertexShaderOutput();
+	}
+	/**
+	 * Returns a list of primitives.
+	 * This function should be implemented in an explicit template specialization.
+	 */
+	static VertexShaderOutputs& assemble(const Primitive, VertexShaderOutputs& outputs) {
+		return outputs;
+	}
+	/**
+	 * Returns an empty list of fragments.
+	 * This function should be implemented in an explicit template specialization.
+	 */
+	static Fragments rasterize(const RenderingContext&, const VertexShaderOutputs&) {
+		return Fragments();
+	}
+	/**
+	 * Returns a pixel value.
+	 * This function should be implemented in an explicit template specialization.
+	 */
+	static std::uint32_t fragmentShader(const Uniforms&, const Varying&, const Fragment&) {
+		return 0xffffffff;
 	}
 };
 } // namespace clockwork
